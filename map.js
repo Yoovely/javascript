c994f13420d73d15cfb97722800fcc6d674a2ed4
YooLee <yookyunglee17@gmail.com>
2022-09-30 16:46:30 -0700

    [update] userList 들여쓰기 업데이트

--- map.js
+++ map.js
@@ -1,8 +1,29 @@
 const userList = [ //array 선언
     // 보통 예제는 배열에 숫자를 담지만, 실무에서는 보통 배열에 object를 담는다 ex. user list
-    {userId:"1", firstName:"Yoolee", lastName:"Lee", yyyymmdd:"20220908", phone:"010-111-1111", address:"Seoul"},
-    {userId:"2", firstName:"Jeremy", lastName:"Jucker", yyyymmdd:"20200303", phone:"010-111-1234", address:"Jeju"},
-    {userId:"3", firstName:"Heesong", lastName:"Lee", yyyymmdd:"19940202", phone:"010-123-1111", address:"Seoul"}
+    {
+        userId:"1",
+        firstName:"Yoolee",
+        lastName:"Lee", 
+        yyyymmdd:"20220908", 
+        phone:"010-111-1111", 
+        address:"Seoul"
+    },
+    {
+        userId:"2", 
+        firstName:"Jeremy", 
+        lastName:"Jucker", 
+        yyyymmdd:"20200303", 
+        phone:"010-111-1234", 
+        address:"Jeju"
+    },
+    {
+        userId:"3", 
+        firstName:"Heesong", 
+        lastName:"Lee", 
+        yyyymmdd:"19940202", 
+        phone:"010-123-1111", 
+        address:"Seoul"
+    }
 ];
 
 var mon = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
